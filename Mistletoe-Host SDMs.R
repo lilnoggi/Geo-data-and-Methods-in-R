79200da451f09da50f24e7273c6129b0820daf63
Stephanie Pearce <stephy.pearce129@gmail.com>
2026-01-06 18:35:57 +0000

    Added region presets with bounds (lon/lat); removed 'here' package
    Added a REGION_PRESETS list, containing bounds for each continent (excluding Antarctica), and a Global option.
    
    Removed 'here' package due to lack of usage.

--- Mistletoe-Host SDMs.R
+++ Mistletoe-Host SDMs.R
@@ -1,14 +1,14 @@
-# ============================================================================================
-#                                       --- START-UP ---
-# ============================================================================================
+# ============================================================================================================
+#                                               --- START-UP ---
+# ============================================================================================================
+
 
 # Install packages for spatial data mapping and handling
 install.packages(c(
   "dplyr",
   "ggplot2",
-  "sysfonts", 
+  "sysfonts",
   "showtext",
-  "here", 
   "terra", 
   "geodata", 
   "rnaturalearth", 
@@ -21,7 +21,6 @@ install.packages(c(
 
 # Load packages
 library(dplyr)
-library(here)
 library(ggplot2)
 library(terra)
 library(geodata)
@@ -33,12 +32,107 @@ library(sysfonts)
 library(showtext)
 
 # Set working directory
-wd <- setwd("C:/University Work/Year 3 Biology/HT/R Coding Assignment/Geo-data and Methods in R - Assignment")
+wd <- setwd("C:/University Work/Year 3 Biology/HT/R Coding Assignment/Geo-data and Methods in R - Assigment")
 
 # Create folders 
 folders <- c("data/raw", "data/processed", "outputs/maps")
 for (f in folders) {
   dir.create(file.path(wd, f), recursive = TRUE, showWarnings = FALSE)
 }
+
+
+# ============================================================================================================
+#                                              --- TASK 1 ---
+# ============================================================================================================
+
+# This section involves running GLMs to  predict the present-day distribution of Species 1
+# and Species 2 using climate variables. Then, these are used to present maps of their
+# current distribution.
+# Here, a function is written for the user to input the two species they are interested in,
+# and what region they want to focus on (Europe, North/South America, Asia, Oceania, Africa).
+
+
+# Region presets with 
+REGION_PRESETS <- list(
+  "Africa"        = c(-20, 55, -40, 40),
+  "Europe"        = c(-25, 60, 34, 72),
+  "South America" = c(-85, -30, -60, 15),
+  "North America" = c(-170, -50, 5, 85),
+  "Oceania"       = c(110, 180, -50, 0),
+  "Asia"          = c(25, 180, -10, 85),
+  "Global"        = c(-180, 180, -90, 90)
+)
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
